0eaa9806e5209f66e21945c40cd2d76edbcedb3e
Federico <fede.magaiza@gmail.com>
2020-08-15 21:15:10 -0300

    login

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ function onSignIn(googleUser) {
   console.log("ID Token: " + id_token);
 }
 
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
 
 document.addEventListener("DOMContentLoaded", function(e){
 
