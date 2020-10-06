fbc7161f3f6277a247b2fa3f3143999825ff637e
Federico <fede.magaiza@gmail.com>
2020-10-06 19:04:19 -0300

    Entrega 5

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,53 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+let subtotal = 0;
+let pre = 0;
+let final = 0;
+
+let sub = document.getElementsByClassName("subtotal");
+
+function precioFinal(){
+    final = 0;
+    for(let i = 0; i < sub.length; i++){
+        final += parseInt(sub[i].innerHTML); 
+    }
+    return final;
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
 
-});
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            let productsList = resultObj.data.articles;
+
+            for(let i = 0; i < productsList.length; i++){
+                subtotal += (productsList[i].unitCost * productsList[i].count)
+                pre = productsList[i].unitCost;
+
+                document.getElementById("tableProducts").innerHTML += `
+                    <tr> 
+                        <td><img class="imgList" src=`+ productsList[i].src +`>`+`</td>
+                        <td>` + productsList[i].name +`</td>
+                        <td class="precio">`+ productsList[i].unitCost +`</td>
+                        <td id="cant"> <input type="number" id="cantidadNum" class="cantidadesCart" min="1" value="`+ productsList[i].count +`"> </td> 
+                        <td>`+ productsList[i].currency +`</td>
+                        <td class="subtotal">`+ subtotal +`</td>
+                    </tr>
+                    `
+                document.getElementById("preFinal").innerHTML = "Total: " + precioFinal();
+                subtotal = 0;
+            }
+        }
+        
+        document.getElementById("aplicarCart").addEventListener('click' , function(e){
+            total = 0;
+
+            let precios = document.getElementsByClassName("precio");
+            let cantidades = document.getElementsByClassName("cantidadesCart");
+
+            for(let i = 0; i < precios.length; i++){
+
+                sub[i].innerHTML = (precios[i].innerHTML * cantidades[i].value);               
+            }
+            document.getElementById("preFinal").innerHTML = "Total: " + precioFinal();
+        })
+    })
+ });
